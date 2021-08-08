0d11e206549db342536baabb4aa81324457d4942
vienthuong <t.le@shopware.com>
2021-08-08 08:13:05 +0700

    Release 1.2.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,20 +4,25 @@ All notable changes to `shopware-php-sdk` will be documented in this file.
 
 Updates should follow the [Keep a CHANGELOG](http://keepachangelog.com/) principles.
 
+### 1.2.2
+- Added SystemConfigService, MailSendService
+- Changed ApiService constructor do not require a $context object, using setContext method instead
+- Fix some minor bugs
+
 ### 1.2.1
-- Add RepositoryFactory::createFromDefinition that allow to generate the repository from an EntityDefinition 
+- Added RepositoryFactory::createFromDefinition that allow to generate the repository from an EntityDefinition 
 
 ### 1.2.0
-- Add ActionButtonResponse classes that can be return directly when receive an action button request
-- Add RegistrationResponse that can be return directly when receive an app register request
-- Update Docs for App
+- Added ActionButtonResponse classes that can be return directly when receive an action button request
+- Added RegistrationResponse that can be return directly when receive an app register request
+- Updated Docs for App
 
 ### 1.1.0
-- Add plugin custom Definition at runtime
-- Update all latest entities from SW 6.4.2.1  
+- Added plugin custom Definition at runtime
+- Updated all latest entities from SW 6.4.2.1  
 - Better Collection generator method annotation
 - HttpClient decoupling
-- Add IframeRequest DTO and ShopRequest constant
+- Added IframeRequest DTO and ShopRequest constant
 
 ### 1.0.0
 - Package initial structure
---
+++ src/Data/Mail/Mail.php
@@ -0,0 +1,194 @@
+<?php declare(strict_types=1);
+
+namespace Vin\ShopwareSdk\Data\Mail;
+
+use Vin\ShopwareSdk\Data\Struct;
+
+class Mail extends Struct
+{
+    protected string $salesChannelId;
+
+    protected string $subject;
+
+    protected string $contentHtml;
+
+    protected string $contentPlain;
+
+    protected string $senderName;
+
+    protected array $recipients;
+
+    protected array $mediaIds;
+
+    protected array $binAttachments;
+
+    protected array $recipientsBcc;
+
+    protected array $recipientsCc;
+
+    protected array $replyTo;
+
+    protected ?string $senderEmail;
+
+    protected ?string $returnPath;
+
+    public function __construct(
+        string $salesChannelId,
+        string $subject,
+        string $contentHtml,
+        string $contentPlain,
+        string $senderName,
+        array $recipients = [],
+        array $mediaIds = [],
+        array $binAttachments = [],
+        array $recipientsBcc = [],
+        array $recipientsCc = [],
+        array $replyTo = [],
+        ?string $senderEmail = null,
+        ?string $returnPath = null
+    ) {
+        $this->salesChannelId = $salesChannelId;
+        $this->subject = $subject;
+        $this->contentHtml = $contentHtml;
+        $this->contentPlain = $contentPlain;
+        $this->senderName = $senderName;
+        $this->recipients = $recipients;
+        $this->mediaIds = $mediaIds;
+        $this->binAttachments = $binAttachments;
+        $this->recipientsBcc = $recipientsBcc;
+        $this->recipientsCc = $recipientsCc;
+        $this->replyTo = $replyTo;
+        $this->senderEmail = $senderEmail;
+        $this->returnPath = $returnPath;
+    }
+
+    public function getSalesChannelId(): string
+    {
+        return $this->salesChannelId;
+    }
+
+    public function setSalesChannelId(string $salesChannelId): void
+    {
+        $this->salesChannelId = $salesChannelId;
+    }
+
+    public function getSubject(): string
+    {
+        return $this->subject;
+    }
+
+    public function setSubject(string $subject): void
+    {
+        $this->subject = $subject;
+    }
+
+    public function getContentHtml(): string
+    {
+        return $this->contentHtml;
+    }
+
+    public function setContentHtml(string $contentHtml): void
+    {
+        $this->contentHtml = $contentHtml;
+    }
+
+    public function getContentPlain(): string
+    {
+        return $this->contentPlain;
+    }
+
+    public function setContentPlain(string $contentPlain): void
+    {
+        $this->contentPlain = $contentPlain;
+    }
+
+    public function getSenderName(): string
+    {
+        return $this->senderName;
+    }
+
+    public function setSenderName(string $senderName): void
+    {
+        $this->senderName = $senderName;
+    }
+
+    public function getRecipients(): array
+    {
+        return $this->recipients;
+    }
+
+    public function setRecipients(array $recipients): void
+    {
+        $this->recipients = $recipients;
+    }
+
+    public function getMediaIds(): array
+    {
+        return $this->mediaIds;
+    }
+
+    public function setMediaIds(array $mediaIds): void
+    {
+        $this->mediaIds = $mediaIds;
+    }
+
+    public function getBinAttachments(): array
+    {
+        return $this->binAttachments;
+    }
+
+    public function setBinAttachments(array $binAttachments): void
+    {
+        $this->binAttachments = $binAttachments;
+    }
+
+    public function getRecipientsBcc(): array
+    {
+        return $this->recipientsBcc;
+    }
+
+    public function setRecipientsBcc(array $recipientsBcc): void
+    {
+        $this->recipientsBcc = $recipientsBcc;
+    }
+
+    public function getRecipientsCc(): array
+    {
+        return $this->recipientsCc;
+    }
+
+    public function setRecipientsCc(array $recipientsCc): void
+    {
+        $this->recipientsCc = $recipientsCc;
+    }
+
+    public function getReplyTo(): array
+    {
+        return $this->replyTo;
+    }
+
+    public function setReplyTo(array $replyTo): void
+    {
+        $this->replyTo = $replyTo;
+    }
+
+    public function getSenderEmail(): ?string
+    {
+        return $this->senderEmail;
+    }
+
+    public function setSenderEmail(?string $senderEmail): void
+    {
+        $this->senderEmail = $senderEmail;
+    }
+
+    public function getReturnPath(): ?string
+    {
+        return $this->returnPath;
+    }
+
+    public function setReturnPath(?string $returnPath): void
+    {
+        $this->returnPath = $returnPath;
+    }
+}
