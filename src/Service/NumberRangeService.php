a062daafc42a717224667011d33ada96d47f9c6c
Dinh Duy <d.dinh@shopware.com>
2023-05-30 21:18:32 +0700

    Added NumberRangeService

---
+++ src/Service/NumberRangeService.php
@@ -0,0 +1,43 @@
+<?php declare(strict_types=1);
+
+namespace Vin\ShopwareSdk\Service;
+
+/**
+ *
+ * Class NumberRangeService
+ * @package Vin\ShopwareSdk\Service
+ */
+class NumberRangeService extends ApiService
+{
+    public function reserve(string $type, ?string $salesChannelId, bool $preview, array $headers = []): ApiResponse
+    {
+        $path = sprintf('/api/_action/number-range/reserve/%s/%s', $type, $salesChannelId ?? '');
+
+        $response = $this->httpClient->get($this->getFullUrl($path), [
+            'query' => [
+                'preview' => $preview,
+            ],
+            'headers' => $this->getBasicHeaders($headers)
+        ]);
+
+        $contents = self::handleResponse($response->getBody()->getContents(), $response->getHeaders());
+
+        return new ApiResponse($contents, $response->getHeaders(), $response->getStatusCode());
+    }
+
+    public function previewPattern(string $type, ?string $pattern, array $headers = []): ApiResponse
+    {
+        $path = sprintf('/api/_action/number-range/preview-pattern/%s', $type);
+
+        $response = $this->httpClient->get($this->getFullUrl($path), [
+            'query' => [
+                'pattern' => $pattern,
+            ],
+            'headers' => $this->getBasicHeaders($headers)
+        ]);
+
+        $contents = self::handleResponse($response->getBody()->getContents(), $response->getHeaders());
+
+        return new ApiResponse($contents, $response->getHeaders(), $response->getStatusCode());
+    }
+}
