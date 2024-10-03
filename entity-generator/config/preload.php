6ccb153c4dde57365b9c66dd6e12aacbdbfb2c7a
Martin Bens <martin.bens@it-bens.de>
2024-10-03 23:37:01 +0200

    Entity generator moved to separate repository (https://github.com/it-bens/shopware-php-sdk-entity-generator).

--- entity-generator/config/preload.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-if (file_exists(dirname(__DIR__).'/var/cache/prod/Vin_ShopwareSdkEntityGenerator_KernelProdContainer.preload.php')) {
-    require dirname(__DIR__).'/var/cache/prod/Vin_ShopwareSdkEntityGenerator_KernelProdContainer.preload.php';
-}
