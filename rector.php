3c66800bdc6af03b1476e63f8d8a60b36c3fe702
Martin Bens <martin.bens@it-bens.de>
2024-10-12 01:24:33 +0200

    `src` and `tests` directory added to Rector configuration.

--- rector.php
+++ rector.php
@@ -6,4 +6,8 @@ use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\LevelSetList;
 
 return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
     ->withSets([LevelSetList::UP_TO_PHP_81]);
